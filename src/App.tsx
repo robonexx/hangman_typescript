7c7339a5cd01442bcb6b40fde459e98e30047e42
robonexx <robertwagar@gmail.com>
2022-11-03 07:46:47 +0100

    adding todo and wordfunction

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import words from "../wordlist.json";
+import "./App.css";
+
+/* 
+todo in project
+create word to guess function using use state (easy version)
+create keyboard
+create guess word "line"
+create hangman or other way of showing amount of guesses
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [wordGuess, setWordGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hangman</h1>
+      <h2>{wordGuess}</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+/* 
+wordlist used in this project
+https://www.randomlists.com/data/words.json 
+*/
